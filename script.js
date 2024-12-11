2085688c91b8f7aa4b2cd65906ee0cfa3738f685
Shalom <saintpius15@gmail.com>
2024-12-11 08:20:42 +0100

    Update script.js
    Added scroll down animation

--- script.js
+++ script.js
@@ -30,3 +30,22 @@ filterOptions.forEach(option => {
     });
   });
 });
+
+// Scroll animation for popping up content
+const animatedItems = document.querySelectorAll("[data-animate]");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+animatedItems.forEach((item) => observer.observe(item));
